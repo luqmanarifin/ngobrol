8f5bea235bf2d212e86a1931867074e0247eb9bd
Husni Munaya <husnimun@gmail.com>
2016-11-02 12:13:05 +0700

    Add register and login [WIP]

--- ngobrol-client/src/com/ngobrol/client/user/service/UserService.java
+++ ngobrol-client/src/com/ngobrol/client/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.ngobrol.client.user.service;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 /**
  * Created by husni on 02/11/16.
  */
@@ -8,11 +11,17 @@ public class UserService {
 
   }
 
-  public void register() {
-
+  public void register(String username, String password) {
+    JSONObject registerMessage = new JSONObject();
+    registerMessage.put("method", "register");
+    registerMessage.put("username", username);
+    registerMessage.put("password", password);
   }
 
-  public void login() {
-
+  public void login(String username, String password) {
+    JSONObject loginMessage = new JSONObject();
+    loginMessage.put("method", "login");
+    loginMessage.put("username", username);
+    loginMessage.put("password", password);
   }
 }
