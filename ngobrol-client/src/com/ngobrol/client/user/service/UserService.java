23018f9af8314a778692be04a7eabc64a50b9f41
Husni Munaya <husnimun@gmail.com>
2016-11-02 12:04:07 +0700

    initial comit for ngobrol-client

---
+++ ngobrol-client/src/com/ngobrol/client/user/service/UserService.java
@@ -0,0 +1,18 @@
+package com.ngobrol.client.user.service;
+
+/**
+ * Created by husni on 02/11/16.
+ */
+public class UserService {
+  public void UserService() {
+
+  }
+
+  public void register() {
+
+  }
+
+  public void login() {
+
+  }
+}
